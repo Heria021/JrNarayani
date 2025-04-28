0b02e7017c5636008dc12f5466d20a14499ac7a3
Heria021 <hariomsuthar7143@gmail.com>
2025-04-28 23:28:13 +0530

    config file updated.

---
+++ next.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pixabay.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      }
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': process.cwd(),
+    };
+    return config;
+  },
+};
+
+export default nextConfig; 
--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['cdn.pixabay.com', 'pixabay.com', 'firebasestorage.googleapis.com', 'res.cloudinary.com'],
-  },
-};
-
-export default nextConfig;
