75334e52d46a9eb170cefd0b17c14b78e56ac27b
Heria021 <hariomsuthar7143@gmail.com>
2025-04-28 23:20:17 +0530

    vercel_bugs_fixes

--- app/dashboard/Client/[clientId]/page.tsx
+++ app/dashboard/Client/[clientId]/page.tsx
@@ -5,7 +5,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
-import PreviewEstimate from "../../estimate/[clientId]/_components/PreviewEstimate";
+import PreviewEstimate from "@/app/dashboard/estimate/[clientId]/_components/PreviewEstimate";
 import { ArrowDownCircleIcon, Wallet } from "lucide-react";
 import {
   Table,
